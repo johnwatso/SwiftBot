c46de434a984a6e376223430374e9099deca63ea
Max Hewett <max@maxhewett.com>
2026-03-02 22:51:15 +1300

    Liquid Glass!

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftBot",
     platforms: [
-        .macOS(.v13)
+        .macOS("26.0")
     ],
     products: [
         .executable(name: "SwiftBot", targets: ["SwiftBot"])
